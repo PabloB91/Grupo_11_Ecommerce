d9ae78973fbbbc111d2aec75d944a2999938e446
FranCapi2005 <ftcapitani@gmail.com>
2024-03-26 21:43:17 -0300

    validate from

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,73 +1,79 @@
-const slider = document.querySelector("#slider--inner");
-let sliderSection = document.querySelectorAll(".img_sliderSection");
-let sliderSectionLast = sliderSection[sliderSection.length - 1];
+document.addEventListener("DOMContentLoaded", function(){
+	const slider = document.querySelector("#slider--inner");
+	if(!slider){
+		console.log("No se encontró el elemento #slider--inner en el DOM.");
+		return; // salir de la función si el elemento no se encontró
+	}
 
-const btnLeft = document.querySelector("#btn-left");
-const btnRight = document.querySelector("#btn-right");
-const btnStop = document.querySelector("#div__btn-pause-play span");
-
-slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-
-function nextLeft() {
-	let sliderSectionFirst = document.querySelectorAll(".img_sliderSection")[0];
-	slider.style.marginLeft = "-200%";
-	slider.style.transition = " 800ms";
-	setTimeout(function () {
-		slider.style.transition = "none";
-		slider.insertAdjacentElement("beforeend", sliderSectionFirst);
-		slider.style.marginLeft = "-100%";
-	}, 800);
-}
-
-function nextRight() {
 	let sliderSection = document.querySelectorAll(".img_sliderSection");
 	let sliderSectionLast = sliderSection[sliderSection.length - 1];
-	slider.style.marginLeft = "0";
-	slider.style.transition = " 800ms";
-	setTimeout(function () {
-		slider.style.transition = "none";
-		slider.insertAdjacentElement("afterbegin", sliderSectionLast);
-		slider.style.marginLeft = "-100%";
-	}, 800);
-}
 
-btnStop.addEventListener("click", function () {
-	const id = btnStop.getAttribute("data-id");
+	const btnLeft = document.querySelector("#btn-left");
+	const btnRight = document.querySelector("#btn-right");
+	const btnStop = document.querySelector("#div__btn-pause-play span");
 
-	//llamar a un servcio
-	//toggleLike(id)
-	if (btnStop.classList.contains("paused")) {
-		btnStop.classList.toggle("paused");
-		btnStop.innerText = "pause";
-		setInterval(function () {}, 15000);
-	} else{
-		btnStop.classList.add("paused");
-		btnStop.innerText = "play_arrow";
-		setInterval(function () {
-			nextRight();
-		}, 5000);
-		setInterval;
+	slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+	function nextLeft() {
+		let sliderSectionFirst = document.querySelectorAll(".img_sliderSection")[0];
+		slider.style.marginLeft = "-200%";
+		slider.style.transition = " 800ms";
+		setTimeout(function () {
+			slider.style.transition = "none";
+			slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+			slider.style.marginLeft = "-100%";
+		}, 800);
 	}
-});
 
-btnRight.addEventListener("click", function () {
-	nextLeft();
-});
+	function nextRight() {
+		let sliderSection = document.querySelectorAll(".img_sliderSection");
+		let sliderSectionLast = sliderSection[sliderSection.length - 1];
+		slider.style.marginLeft = "0";
+		slider.style.transition = " 800ms";
+		setTimeout(function () {
+			slider.style.transition = "none";
+			slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+			slider.style.marginLeft = "-100%";
+		}, 800);
+	}
 
-btnLeft.addEventListener("click", function () {
-	nextRight();
-});
+	btnStop.addEventListener("click", function () {
+		const id = btnStop.getAttribute("data-id");
 
-/**Funcion para las flechas de desplazamiento delas cards */
-const rightBtnSliderCards = document.querySelector("div#btnsCards-right");
-const leftBtnSliderCards = document.querySelector("div#btnsCards-left");
-const containerCards = document.querySelector("#div_cards--scrolling");
+		//llamar a un servcio
+		//toggleLike(id)
+		if (btnStop.classList.contains("paused")) {
+			btnStop.classList.toggle("paused");
+			btnStop.innerText = "pause";
+			setInterval(function () {}, 15000);
+		} else{
+			btnStop.classList.add("paused");
+			btnStop.innerText = "play_arrow";
+			setInterval(function () {
+				nextRight();
+			}, 5000);
+			setInterval;
+		}
+	});
 
-//scroll left
-rightBtnSliderCards.addEventListener("click", () => {
-	containerCards.scrollLeft += 800;
-});
-leftBtnSliderCards.addEventListener("click", () => {
-	containerCards.scrollLeft -= 800;
-});
+	btnRight.addEventListener("click", function () {
+		nextLeft();
+	});
 
+	btnLeft.addEventListener("click", function () {
+		nextRight();
+	});
+
+	/**Funcion para las flechas de desplazamiento delas cards */
+	const rightBtnSliderCards = document.querySelector("div#btnsCards-right");
+	const leftBtnSliderCards = document.querySelector("div#btnsCards-left");
+	const containerCards = document.querySelector("#div_cards--scrolling");
+
+	//scroll left
+	rightBtnSliderCards.addEventListener("click", () => {
+		containerCards.scrollLeft += 800;
+	});
+	leftBtnSliderCards.addEventListener("click", () => {
+		containerCards.scrollLeft -= 800;
+	});
+})
--- public/scripts/registerV.js
+++ public/scripts/registerV.js
@@ -1,26 +1,30 @@
 window.addEventListener("load", function(){
 
-    let form = document.querySelector(".main-login_form form")
+    let form = document.querySelector(".main-login_form")
 
     form.addEventListener("submit", function(e){
-
+        /* Array que contendra errores */
         const errores = [];
 
+        /* Validacion de Nombre */
         const name = document.getElementById("user-name");
         if(name.value === ""){
             errores.push("El nombre no puede estar vacío.")
         };
 
+        /* Validacion de Apellido */
         const last_name = document.getElementById("last-name");
         if(last_name.value === ""){
             errores.push("El apellido no puede estar vacío.")
         };
 
+        /* Validacion de Email */
         const email = document.getElementById("e-mail");
         if(email.value === ""){
             errores.push("El email no puede estar vacío.");
         }
 
+        /* Validacion de contraseña */
         const password = document.getElementById("password");
 
             const hasUppercase = /[A-Z]/;
@@ -28,23 +32,34 @@ window.addEventListener("load", function(){
             const hasLowercase = /[a-z]/;
 
         if(password.value === ""){
-            errores.push("El password no puede estar vacío.");
+            
+            errores.push("La contraseña no puede estar vacía.");
+
         }else if(password.length < 8){
+            
             errores.push("La contraseña debe tener al menos 8 caracteres");
+        
         }else if(!password.value.includes(hasUppercase)){
+        
             errores.push("La contraseña debe tener un al menos una mayuscula.")
+        
         }else if(!password.value.includes(hasLowercase)){
+        
             errores.push("La contraseña debe tener un al menos una minuscula.")
+        
         }else if(!password.value.includes(hasSpecialCharacter)){
+        
             errores.push("La contraseña debe tener un al menos un caracter especial.")
+        
         };
 
+        /* Validacion Confirmacion de Contraseña */
         const confirmPassword = document.getElementById("confirm-password");
         if(confirmPassword.value !== password){
             errores.push("La contraseña debe ser la misma")
         }
 
-
+        /* Condicional errores */
         if(errores.length > 0){
             e.preventDefault();
 
@@ -56,3 +71,5 @@ window.addEventListener("load", function(){
         }
     })
 })
+
+
