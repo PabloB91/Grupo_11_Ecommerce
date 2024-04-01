e481db2e104a029dc8e42a186021afe6a6b54ee0
Jscinteractivo <jscinteractivo@gmail.com>
2024-03-31 22:41:18 -0500

    Adaptación de vista guía momentánea de todas las marcas SOUNDBOX (base paragenerar la vista dinámica 'brandsList.ejs') / Adaptación de Hoja de Estilos (brands.css) para la vista general de las marcas SBX / proceso de mejoras visuales en vista allTheproducts.ejs (se cambió titular por fuente Bebas Neuve en 36px, checkboxes de ventana flotante de filtros redondeados y adaptado según paleta de colores (al activar filtro se ve naranja en vez de azul) / entre otros...

--- src/controllers/brandsController.js
+++ src/controllers/brandsController.js
@@ -1,9 +1,5 @@
 //-------- CONTROLADOR MARCAS SOUNDBOX --------//
 
-// OJO: revisar en general el tema del idioma. Estamos utilizando Espanglish en todo el proyecto.
-
-// OJO: pendiente concretar el nombre del .ejs de las marcas: ¿ brandsList o simplmnte brands.ejs considerando que según lo acordado en grupo solo generaremos una vista con los logos de las marcas. Por ahora no vamos a generar vistas adicionales de los productos que corresponden a cada marca.
-
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -13,9 +9,9 @@ const db = require("../database/models");
 const brandsController = {
 	listBrands: async (req, res) => {
 		try {
-			let brands = await db.Marcas.findAll(); //OJO recorderis: la tabla se llama 'brand' y el alias es 'Marcas'
+			let brands = await db.Marcas.findAll();
 			res.render(
-				"product/brandsList.ejs" /* OJO: leer el OJO de arriba */,
+				"product/brandsList.ejs",
 				{ brands }
 			);
 		} catch (err) {
