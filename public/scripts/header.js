29fac1e128292b352ca8346d930da529e334619f
Miguel vargas Navarro <mvargasnavarro19@gmail.com>
2024-04-08 15:22:19 -0500

    comente el codigo del header para que no moleste en la vista

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,9 +1,9 @@
-const logoSBX = document.querySelector('#img-logoSBX');
+/* const logoSBX = document.querySelector('#img-logoSBX');
 function fixed () {
 let headerBottomAnimation  = document.querySelector('.headerBottom');
 headerBottomAnimation.classList.toggle('fixed', window.scrollY), 500;
 };
-window.addEventListener('scroll', fixed);
+window.addEventListener('scroll', fixed); */
 /*  for(let i = 0; i < animationShowBoxSb.length; i++ ){
 
  }*/
